5d02b1c72031d23dc4cfd0a44901d408c6270e23
brianphil <brianphilemon@gmail.com>
2023-03-22 15:26:24 +0300

    Initial commit

---
+++ src/components/Table.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const Table = () => {
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th>Serial</th>
+          <th>Sample</th>
+          <th>Type</th>
+          <th>Pattern</th>
+          <th>Length</th>
+          <th>Scheme</th>
+          <th>Cost</th>
+          <th>Return</th>
+          <th>Initial State</th>
+          <th>List</th>
+          <th>Date</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Serial</td>
+          <td>Sample</td>
+          <td>Type</td>
+          <td>Pattern</td>
+          <td>Length</td>
+          <td>Scheme</td>
+          <td>Cost</td>
+          <td>Return</td>
+          <td>Initial State</td>
+          <td>List</td>
+          <td>Date</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
+export default Table;
